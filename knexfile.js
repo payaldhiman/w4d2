f66f338bec45cefbcc5c6483b4ec39ba063fc74c
Payal Dhiman <payaldhiman@outlook.com>
2017-12-20 02:23:49 +0000

    Database Migrations Assignment

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+const settings = require("./settings"); // settings.json
+
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      // filename: './settings.json'
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
+    }
+  },
+}
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
+// };
